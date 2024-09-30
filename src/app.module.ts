5204bcdbad150c8ada5e9591c77d4beb5ae8345f
Javier Godoy Núñez <javier@oxozon.io>
2024-09-29 21:20:13 -0400

    landed: Using .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
+/**
+ * The main application module.
+ *
+ * This module imports the configuration module, sets up the application controller,
+ * and provides the application service.
+ *
+ * @module AppModule
+ */
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
