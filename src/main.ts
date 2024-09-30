63f7382e7b800a682617f6f3817fb3a7c5449534
Javier Godoy Núñez <javier@oxozon.io>
2024-09-29 20:54:41 -0400

    landed: Using fastify instead of express

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+/**
+ * Initializes and starts the NestJS application using the Fastify adapter.
+ *
+ * This function creates an instance of the NestJS application with the specified
+ * module and adapter, and then listens on port 3000.
+ *
+ * @async
+ * @function bootstrap
+ * @returns {Promise<void>} A promise that resolves when the application has started.
+ */
+const bootstrap = async () => {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   await app.listen(3000);
-}
+};
+
 bootstrap();
