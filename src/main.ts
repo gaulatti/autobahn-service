1898e01cfc380558edd5e499b8f8824504323251
Javier Godoy <rotvulpix@gmail.com>
2024-10-07 16:18:21 -0400

    wip: Attempt to dual-run local and in lambda

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,20 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { AuthorizationGuard } from './authorization/authorization.guard';
+import { INestApplication } from '@nestjs/common';
 
 /**
- * Initializes and starts the NestJS application using the Fastify adapter.
+ * Initializes and configures the NestJS application using the Fastify adapter.
  *
- * This function creates an instance of the NestJS application with the specified
- * module and adapter, and then listens on port 3000.
+ * - Creates an instance of the NestJS application.
+ * - Enables Cross-Origin Resource Sharing (CORS).
+ * - Sets the global guard to `AuthorizationGuard`.
+ * - If the module is the main module, starts the application on port 3000.
  *
- * @async
- * @function bootstrap
- * @returns {Promise<void>} A promise that resolves when the application has started.
+ * @returns {Promise<void | INestApplication>} A promise that resolves when the application is started or returns the application instance.
  */
-const bootstrap = async (): Promise<void> => {
+const bootstrap = async (): Promise<void | INestApplication> => {
+  console.log(require.main === module);
   /**
    * Creates an instance of the NestJS application using the Fastify adapter.
    */
@@ -36,9 +38,23 @@ const bootstrap = async (): Promise<void> => {
   app.useGlobalGuards(new AuthorizationGuard());
 
   /**
-   * Starts the application on port 3000.
+   * If the module is the main module, starts the application on port 3000.
+   * This allows dual usage, either locally or within Lambda.
    */
-  await app.listen(3000);
+  if (require.main === module) {
+    /**
+     * Starts the application on port 3000.
+     */
+    await app.listen(3000, (err) => {
+      if (err) console.error(err);
+      console.log('Madonna listening on 3000');
+    });
+  } else {
+    /**
+     * Returns the application instance.
+     */
+    return app;
+  }
 };
 
 bootstrap();
