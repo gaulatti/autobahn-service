f72a633fddeeaf2105705e051877e69bc9a8df7f
Javier Godoy <rotvulpix@gmail.com>
2024-10-08 09:14:03 -0400

    landed: Retry heartbeat

--- src/assessments/heartbeats/heartbeats.controller.ts
+++ src/assessments/heartbeats/heartbeats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Query } from '@nestjs/common';
 import { HeartbeatsService } from './heartbeats.service';
 
 @Controller('pulses/:uuid')
@@ -7,11 +7,37 @@ export class HeartbeatsController {
 
   @Get('mobile')
   getHeartbeatMobile(@Param('uuid') uuid: string) {
-    return this.heartbeatsService.getHeartbeatJSON(uuid, true);
+    return this.heartbeatsService.getHeartbeat(uuid, true);
   }
 
   @Get('desktop')
   getHeartbeatDesktop(@Param('uuid') uuid: string) {
-    return this.heartbeatsService.getHeartbeatJSON(uuid);
+    return this.heartbeatsService.getHeartbeat(uuid);
+  }
+
+  @Get('mobile/json')
+  getHeartbeatMobileJSON(
+    @Param('uuid') uuid: string,
+    @Query('minified') minified: boolean = false,
+  ) {
+    return this.heartbeatsService.getHeartbeatJSON(uuid, true, minified);
+  }
+
+  @Get('desktop/json')
+  getHeartbeatDesktopJSON(
+    @Param('uuid') uuid: string,
+    @Query('minified') minified: boolean = false,
+  ) {
+    return this.heartbeatsService.getHeartbeatJSON(uuid, false, minified);
+  }
+
+  @Patch('mobile/retry')
+  retryHeartbeatMobile(@Param('uuid') uuid: string) {
+    return this.heartbeatsService.retryHeartbeat(uuid, true);
+  }
+
+  @Patch('desktop/retry')
+  retryHeartbeatDesktop(@Param('uuid') uuid: string) {
+    return this.heartbeatsService.retryHeartbeat(uuid, true);
   }
 }
