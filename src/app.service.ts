e88f4b7b5ba07e655ea363233721e5173208466c
Javier Godoy <rotvulpix@gmail.com>
2024-10-08 11:00:01 -0400

    chore: Adding membership and team to kickoff response

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './models/user.model';
 import { ENUMS } from './consts';
+import { Membership } from './models/membership.model';
+import { Team } from './models/team.model';
 
 @Injectable()
 export class AppService {
@@ -17,7 +19,10 @@ export class AppService {
    * @returns A promise that resolves to an object containing enums, the user data, and an empty features array.
    */
   async kickoff(user: { userId: string }) {
-    const me = await this.user.findOne({ where: { sub: user.userId } });
+    const me = await this.user.findOne({
+      where: { sub: user.userId },
+      include: [{ model: Membership, include: [{ model: Team }] }],
+    });
     return { enums: ENUMS, me, features: [] };
   }
 }
