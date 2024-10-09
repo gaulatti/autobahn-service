b938d47b16c3ea98f72dfcaee13df476dab54b71
Javier Godoy <rotvulpix@gmail.com>
2024-10-09 15:58:19 -0400

    wip: Targets

--- src/assessments/targets/targets.controller.ts
+++ src/assessments/targets/targets.controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { PulsesService } from '../pulses/pulses.service';
+import { SchedulesService } from '../schedules/schedules.service';
+import { UrlsService } from '../urls/urls.service';
+import { TargetsService } from './targets.service';
+import { TargetsDto } from './targets.dto';
 
 @Controller('targets')
-export class TargetsController {}
+export class TargetsController {
+  constructor(
+    private readonly targetsService: TargetsService,
+    private readonly pulsesService: PulsesService,
+    private readonly urlsService: UrlsService,
+    private readonly schedulesService: SchedulesService,
+  ) {}
+
+  @Get()
+  async allTargets() {
+    return this.targetsService.allTargets();
+  }
+
+  @Post()
+  async createTarget(@Body() dto: TargetsDto) {
+    return this.targetsService.createTarget(dto);
+  }
+
+  @Get(':uuid')
+  async getTarget(@Param('uuid') uuid: string) {
+    return this.targetsService.getTarget(uuid);
+  }
+
+  @Patch(':uuid')
+  async updateTarget(@Param('uuid') uuid: string, @Body() dto: TargetsDto) {
+    return this.targetsService.updateTarget(uuid, dto);
+  }
+
+  @Delete(':uuid')
+  async deleteTarget(@Param('uuid') uuid: string) {
+    return this.targetsService.deleteTarget(uuid);
+  }
+
+  @Get(':uuid/pulses')
+  async pulses(@Param('uuid') uuid: string) {
+    return this.pulsesService.pulsesByTarget(uuid);
+  }
+
+  @Get(':uuid/schedules')
+  async schedules(@Param('uuid') uuid: string) {
+    return this.schedulesService.schedulesByTarget(uuid);
+  }
+
+  @Get(':uuid/urls')
+  async urls(@Param('uuid') uuid: string) {
+    return this.urlsService.urlsByTarget(uuid);
+  }
+}
--- src/assessments/targets/targets.service.ts
+++ src/assessments/targets/targets.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { TargetsDto } from './targets.dto';
 
 @Injectable()
-export class TargetsService {}
+export class TargetsService {
+  deleteTarget(uuid: string) {
+    throw new Error('Method not implemented.');
+  }
+  updateTarget(uuid: string, dto: TargetsDto) {
+    throw new Error('Method not implemented.');
+  }
+  createTarget(dto: TargetsDto) {
+    throw new Error('Method not implemented.');
+  }
+  allTargets() {
+    throw new Error('Method not implemented.');
+  }
+  getTarget(uuid: string) {
+    throw new Error('Method not implemented.');
+  }
+  targetsByUrl(uuid: string) {
+    throw new Error('Method not implemented.');
+  }
+}
