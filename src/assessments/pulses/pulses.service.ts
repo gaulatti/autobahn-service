1365f2c52b8bed954860b01604c0e2ce630df072
Javier Godoy <rotvulpix@gmail.com>
2024-10-09 15:58:01 -0400

    wip: Pulses

--- src/assessments/pulses/pulses.service.ts
+++ src/assessments/pulses/pulses.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { Op } from 'sequelize';
 import { Heartbeat } from 'src/models/heartbeat.model';
 import { Membership } from 'src/models/membership.model';
 import { Project } from 'src/models/project.model';
@@ -8,6 +9,7 @@ import { Schedule } from 'src/models/schedule.model';
 import { Target } from 'src/models/target.model';
 import { Url } from 'src/models/url.model';
 import { User } from 'src/models/user.model';
+import { getPaginationParams } from 'src/utils/lists';
 
 @Injectable()
 export class PulsesService {
@@ -15,6 +17,41 @@ export class PulsesService {
     @InjectModel(Pulse)
     private readonly pulse: typeof Pulse,
   ) {}
+
+  pulsesByTarget(uuid: string) {
+    throw new Error('Method not implemented.');
+  }
+
+  /**
+   * Retrieves pulses by URL ID within a specified date range and pagination parameters.
+   *
+   * @param id - The URL ID to filter pulses.
+   * @param from - The start date of the date range (optional).
+   * @param to - The end date of the date range (optional).
+   * @param startRow - The starting row for pagination (optional).
+   * @param endRow - The ending row for pagination (optional).
+   * @returns A promise that resolves to the list of pulses matching the criteria.
+   */
+  pulsesByUrl(
+    id: number,
+    from?: Date,
+    to?: Date,
+    startRow?: number,
+    endRow?: number,
+  ) {
+    const paginationParams = getPaginationParams(startRow, endRow);
+
+    return this.pulse.findAll({
+      ...paginationParams,
+      where: {
+        urlId: id,
+        createdAt: {
+          [Op.between]: [from, to],
+        },
+      },
+      include: [{ model: Heartbeat, as: 'heartbeats' }],
+    });
+  }
 
   /**
    * Retrieves all pulses along with their associated Heartbeat, Url, Target,
