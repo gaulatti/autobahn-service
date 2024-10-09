3a47749257ee1aad5eb173f1263734f17719574a
Javier Godoy <rotvulpix@gmail.com>
2024-10-09 15:58:23 -0400

    wip: Urls

--- src/assessments/urls/urls.controller.ts
+++ src/assessments/urls/urls.controller.ts
@@ -1,12 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { UrlsService } from './urls.service';
+import { TargetsService } from '../targets/targets.service';
+import { PulsesService } from '../pulses/pulses.service';
 
 @Controller('urls')
 export class UrlsController {
-  constructor(private readonly urlsService: UrlsService) {}
+  constructor(
+    private readonly urlsService: UrlsService,
+    private readonly targetsService: TargetsService,
+    private readonly pulsesService: PulsesService,
+  ) {}
 
   @Get()
   async allUrls() {
     return this.urlsService.allUrls();
+  }
+
+  @Get(':uuid/stats')
+  async stats(
+    @Param('uuid') uuid: string,
+    @Query('from') from: Date,
+    @Query('to') to: Date,
+  ) {
+    return this.urlsService.urlStats(uuid, from, to);
+  }
+
+  @Get(':uuid/pulses')
+  async pulses(
+    @Param('uuid') uuid: string,
+    @Query('from') from: Date,
+    @Query('to') to: Date,
+    @Query('startRow') startRow: number,
+    @Query('endRow') endRow: number,
+    @Query('sort') sort: string,
+    @Query('filters') filters: string,
+  ) {
+    const url = await this.urlsService.urlByUUID(uuid);
+    return this.pulsesService.pulsesByUrl(url.id, from, to, startRow, endRow);
+  }
+
+  @Get(':uuid/targets')
+  async targets(@Param('uuid') uuid: string) {
+    return this.targetsService.targetsByUrl(uuid);
   }
 }
--- src/assessments/urls/urls.service.ts
+++ src/assessments/urls/urls.service.ts
@@ -1,12 +1,53 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Url } from 'src/models/url.model';
+import { PulsesService } from '../pulses/pulses.service';
+import { calculateCWVStats, calculateScores } from 'src/utils/stats';
 
 @Injectable()
 export class UrlsService {
-  constructor(@InjectModel(Url) private readonly url: typeof Url) {}
+  constructor(
+    @InjectModel(Url) private readonly url: typeof Url,
+    private readonly pulsesService: PulsesService,
+  ) {}
 
   async allUrls() {
     return this.url.findAndCountAll();
+  }
+
+  async urlByUUID(uuid: string) {
+    return this.url.findOne({ where: { uuid } });
+  }
+
+  urlsByTarget(uuid: string) {
+    throw new Error('Method not implemented.');
+  }
+
+  async urlStats(uuid: string, from: Date, to: Date) {
+    /**
+     * Fetch the URL record by UUID
+     */
+    const url = await this.urlByUUID(uuid);
+
+    /**
+     * Fetch the pulses for the provided URL
+     */
+    const pulses = await this.pulsesService.pulsesByUrl(url.id, from, to);
+
+    /**
+     * Filter out pulses that don't have any completed heartbeats
+     */
+    const statPulses = pulses.filter(
+      (pulse: { heartbeats: { status: number }[] }) =>
+        pulse.heartbeats.every((heartbeat) => heartbeat.status === 4),
+    );
+
+    /**
+     * Calculate the scores and CWV stats for the provided pulses
+     */
+    const scores = calculateScores(statPulses);
+    const cwvStats = calculateCWVStats(statPulses);
+
+    return { url, cwvStats, scores };
   }
 }
