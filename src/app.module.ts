6e383305cac1c4ec55696f27d07f9895ee1f3e70
Javier Godoy <rotvulpix@gmail.com>
2024-12-24 19:06:33 -0500

    landed: Sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,83 @@
+import {
+  GetSecretValueCommand,
+  SecretsManagerClient,
+} from '@aws-sdk/client-secrets-manager';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { SequelizeModule, SequelizeModuleOptions } from '@nestjs/sequelize';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorizationModule } from './authorization/authorization.module';
+import { CloudWatchService } from './core/cloudwatch/cloudwatch.service';
 import { CoreModule } from './core/core.module';
+import { MetricsInterceptor } from './core/metrics/metrics.interceptor';
 import { DalModule } from './dal/dal.module';
 import { SettingsModule } from './settings/settings.module';
-import { CloudWatchService } from './core/cloudwatch/cloudwatch.service';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { MetricsInterceptor } from './core/metrics/metrics.interceptor';
+
+/**
+ * The AWS Secrets Manager client.
+ */
+const secretsManager = new SecretsManagerClient();
+
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const defaultConfig: SequelizeModuleOptions = {
+          dialect: 'mysql',
+          port: +3306,
+          models: [join(__dirname, '**/*.model.ts')],
+          autoLoadModels: true,
+        };
+
+        if (configService.get('USE_LOCAL_DATABASE') === 'true') {
+          return {
+            ...defaultConfig,
+            host: configService.get('DB_HOST'),
+            port: +configService.get('DB_PORT'),
+            username: configService.get('DB_USERNAME'),
+            password: configService.get('DB_PASSWORD'),
+            database: configService.get('DB_DATABASE'),
+          };
+        }
+
+        /**
+         * Retrieve the secret from AWS Secrets Manager.
+         */
+        const secretResponse = await secretsManager.send(
+          new GetSecretValueCommand({
+            SecretId: configService.get('DB_CREDENTIALS'),
+          }),
+        );
+
+        /**
+         * If the secret response contains a secret string, parse it and return the database configuration.
+         */
+        if (secretResponse.SecretString) {
+          const { host, port, username, password, database } = JSON.parse(
+            secretResponse.SecretString,
+          );
+
+          const remoteConfig = {
+            ...defaultConfig,
+            host: host,
+            port: +port,
+            username,
+            password,
+            database,
+          };
+
+          return {
+            ...remoteConfig,
+          };
+        }
+      },
+      inject: [ConfigService],
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
     CoreModule,
     DalModule,
